386134a660a8e73b289e84c2da51e4183de2a71b
Elizabeth Li <sincerelylibby@gmail.com>
2018-06-25 19:26:38 -0700

    Create data generation script for Cassandra.

--- data/generateDataCassandra.js
+++ data/generateDataCassandra.js
@@ -6,27 +6,54 @@ const stream = require('stream');
 const chance = new Chance();
 
 const createFile = (streamWriter, data, entries) => {
-  let i = entries;
+  let i = 0;
   const write = () => {
     let drained = true;
     do {
-      i -= 1;
+      i += 1;
       drained = streamWriter.write(data());
-    } while (i > 0 && drained);
-    if (i > 0) {
+    } while (i < entries && drained);
+    if (i < entries) {
       streamWriter.once('drain', write);
     }
   };
   write();
 };
 
-const createFileFromArray = (streamWriter, data) => {
+const createRestaurantNamesArray = (num) => {
+  const restaurantNamesArray = [];
+  for (let i = 0; i < num; i += 1) {
+    restaurantNamesArray.push(faker.lorem.words());
+  }
+  return restaurantNamesArray;
+};
+
+const createRestaurantItemNamesArray = (num) => {
+  const restaurantItemNames = [];
+  for (let i = 0; i < num; i += 1) {
+    restaurantItemNames.push(`${faker.lorem.words()}`);
+  }
+  return restaurantItemNames;
+};
+
+const restaurantNames = createRestaurantNamesArray(1000000);
+const restaurantItemNames = createRestaurantItemNamesArray(10);
+
+const createRestaurantItemsFile = (streamWriter, data) => {
   let i = 0;
   const write = () => {
     let drained = true;
     do {
-      drained = streamWriter.write(`${data[i]}\n`);
       i += 1;
+      for (let j = 0; j < 15; j += 1) {
+        drained = streamWriter.write(
+          `${data[i]},`
+          + `${restaurantItemNames[Math.floor(Math.random() * restaurantItemNames.length)]},`
+          + `${faker.lorem.sentence()},`
+          + `${chance.floating({ min: 0, max: 200, fixed: 2 })},`
+          + `${`https://s3-us-west-1.amazonaws.com/sdc-datatable/menu-item${Math.floor(Math.random() * 1035) + 1}.jpg`}\n`
+        );
+      }
     } while (i < data.length && drained);
     if (i < data.length) {
       streamWriter.once('drain', write);
@@ -35,75 +62,54 @@ const createFileFromArray = (streamWriter, data) => {
   write();
 };
 
-const createItemArray = (num) => {
-  const itemArray = [];
-  for (let i = 0; i < num; i += 1) {
-    itemArray.push(faker.lorem.words());
-  }
-  return itemArray;
+
+const createDietaryRestrictionsFile = () => {
+  const dietaryRestrictionsArray = ['Vegetarian', 'Vegan', 'Gluten-Free', 'No Seafood', 'No Peanuts'];
+  const generateDietaryRestrictionsData = () => 
+    `${restaurantItemNames[Math.floor(Math.random() * restaurantItemNames.length)]},`
+      + `${dietaryRestrictionsArray[Math.floor(Math.random() * dietaryRestrictionsArray.length)]}\n`;
+  createFile(
+    fs.createWriteStream('./data/files/cass_restrictions.csv'),
+    generateDietaryRestrictionsData,
+    1000);
 };
 
-const restaurantItems = createItemArray(1000);
-
-const createRestaurantsFile = () => {
-  const generateRestaurantData = () =>
-    `${faker.lorem.words()},`
-    + `${restaurantItems[Math.floor(Math.random() * 1000) + 1]},`
-    + `${faker.lorem.sentence()},`
-    + `${chance.floating({ min: 0, max: 200, fixed: 2 })},`
-    + `${`https://s3-us-west-1.amazonaws.com/sdc-datatable/menu-item${Math.floor(Math.random() * 1035) + 1}.jpg`}\n`;
-  createFile(fs.createWriteStream('./data/files/cass_restaurants.csv'), generateRestaurantData, 10000000);
+const createMenuSectionsFile = () => {
+  const menuSectionsArray = ['Breakfast', 'Lunch', 'Dinner', 'Brunch', 'Desserts', 'Drinks', 'Happy Hour'];
+  const generateMenuSectionsData = () => 
+    `${menuSectionsArray[Math.floor(Math.random() * menuSectionsArray.length)]},`
+    + `${restaurantItemNames[Math.floor(Math.random() * restaurantItemNames.length)]}\n`
+  createFile(
+    fs.createWriteStream('./data/files/cass_sections.csv'),
+    generateMenuSectionsData,
+    1000);
 };
 
-const createRestrictionsFile = () => {
-  const restrictionsArray = ['Vegetarian', 'Vegan', 'Gluten-Free', 'No Seafood', 'No Peanuts'];
-  createFileFromArray(fs.createWriteStream('./data/files/restrictions.csv'), restrictionsArray);
-};
-
-const sectionArray = ['Breakfast', 'Lunch', 'Dinner', 'Brunch', 'Desserts', 'Drinks', 'Happy Hour'];
-const sectionsStream = fs.createWriteStream('./data/files/sections.csv');
-createFileFromArray(sectionsStream, sectionArray);
-
-// create file for item_restrictions table
-const itemRestrictionsStream = fs.createWriteStream('./data/files/item_restrictions.csv');
-const generateItemRestrictionsData = () => `${Math.floor(Math.random() * (1000)) + 1},${Math.floor(Math.random() * restrictionsArray.length + 1)}\n`;
-createFile(itemRestrictionsStream, generateItemRestrictionsData, 1000);
-
-// create file for item_section table
-const itemSectionStream = fs.createWriteStream('./data/files/item_section.csv');
-const generateItemSectionData = () => (Math.floor(Math.random() * sectionArray.length) + 1);
-const createItemSectionFile = (streamWriter, data, entries) => {
+const createRestaurantHeadersFile = (streamWriter, data) => {
   let i = 0;
   const write = () => {
     let drained = true;
     do {
       i += 1;
-      drained = streamWriter.write(`${i},${data()}\n`);
-    } while (i < entries && drained);
-    if (i < entries) {
-      streamWriter.once('drain', write);
-    }
-  };
-  write();
-};
-createItemSectionFile(itemSectionStream, generateItemSectionData, 1000);
-
-// create file for restaurant_items table
-const restaurantItemsStream = fs.createWriteStream('./data/files/restaurant_items.csv');
-const createRestaurantItemsFile = (streamWriter, entries) => {
-  let i = 0;
-  const write = () => {
-    let drained = true;
-    do {
-      i += 1;
-      for (let j = 1; j < 16; j += 1) {
-        drained = streamWriter.write(`${i},${Math.floor(Math.random() * 1000) + 1}\n`);
+      for (let j = 0; j < 3; j += 1) {
+        drained = streamWriter.write(
+          `${data[i]},`
+          + `${faker.lorem.word()}\n`
+        );
       }
-    } while (i < entries && drained);
-    if (i < entries) {
+    } while (i < data.length && drained);
+    if (i < data.length) {
       streamWriter.once('drain', write);
     }
   };
   write();
 };
-createRestaurantItemsFile(restaurantItemsStream, 10000000);
+
+const generateAllData = () => {
+  createRestaurantItemsFile(fs.createWriteStream('./data/files/cass_restaurants.csv'), restaurantNames);
+  createDietaryRestrictionsFile();
+  createMenuSectionsFile();
+  createRestaurantHeadersFile(fs.createWriteStream('./data/files/cass_headers.csv'), restaurantNames);
+};
+
+generateAllData();
