756e58c2aa10da10f2fed6c64b6943959265263b
Elizabeth Li <sincerelylibby@gmail.com>
2018-06-30 11:05:38 -0700

    Implement test for get request.

--- test/test-server.js
+++ test/test-server.js
@@ -1,20 +1,27 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../server/application.js');
-const should = chai.should();
 
+const should = chai.should();
 chai.use(chaiHttp);
 
 describe('CRUD operations', () => {
-  it('should successfully perform a GET request', done => {
+  it('should retrieve the menu data for restaurant 1 on GET request to that endpoint', done => {
     chai.request(server)
     .get('/menus/restaurant/1/menu')
     .end((err, res) => {
       res.should.have.status(200);
+      res.should.be.json;
+      res.body.should.be.a('object');
+      res.body.command.should.equal('SELECT');
       done();
     });
   });
-  it('should perform a POST request');
+  it('should add an item to the menu_items table on POST request to that endpoint', done => {
+    chai.request(server)
+      .post('/menus/restaurant/1/menu')
+      .end()
+  });
   it('should provide a PUT request');
   it('should provide a DELETE request');
 });
