b4109023a0096312dcc5b6452535611d364bce07
Elizabeth Li <sincerelylibby@gmail.com>
2018-06-30 12:48:10 -0700

    Implement tests for GET and POST requests.

--- test/server-tests.js
+++ test/server-tests.js
@@ -8,19 +8,30 @@ chai.use(chaiHttp);
 describe('CRUD operations', () => {
   it('should retrieve the menu data for restaurant 1 on GET request to that endpoint', done => {
     chai.request(server)
-    .get('/menus/restaurant/1/menu')
-    .end((err, res) => {
-      res.should.have.status(200);
-      res.should.be.json;
-      res.body.should.be.a('object');
-      res.body.command.should.equal('SELECT');
-      done();
-    });
+      .get('/menus/restaurant/1/menu')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.command.should.equal('SELECT');
+        done();
+      });
   });
   it('should add an item to the menu_items table on POST request to that endpoint', done => {
     chai.request(server)
       .post('/menus/restaurant/1/menu')
-      .end()
+      .send({
+        item_name: "Chai Latte",
+        item_description: "With hints of mocha",
+        item_price: 3.00,
+        section_name: "Breakfast",
+        restriction_name: "Vegetarian"
+      })
+      .end((err, res) => {
+      res.should.have.status(201);
+      res.body.should.equal('Data successfully posted');
+      done();
+      });
   });
   it('should provide a PUT request');
   it('should provide a DELETE request');
